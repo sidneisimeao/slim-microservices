55bb2554f054f0b5a3c6a1d739c540d2b54ebd29
Sidnei Simmon <sidneisimmon@gmail.com>
2018-12-19 14:35:32 -0200

    Slim Framework — Criando Microservices 04 — CRUD completo via API com Doctrine

--- bootstrap.php
+++ bootstrap.php
@@ -1,20 +1,38 @@
 <?php
 require './vendor/autoload.php';
 
-use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Monolog\Logger;
+
 /**
  * Container Resources do Slim.
  * Aqui dentro dele vamos carregar todas as dependências
  * da nossa aplicação que vão ser consumidas durante a execução
  * da nossa API
  */
-$container = new \Slim\Container();
-$isDevMode = true;
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'logger' => [
+            'name' => 'slim-app',
+            'level' => Logger::DEBUG,
+            'path' => __DIR__ . "/src/logs/app.log",
+        ],
+    ],
+];
+
+$container = new \Slim\Container($configuration);
+
+$debug = true;
+$paths = array(__DIR__."/src/Models/Entity");
+
 /**
  * Diretório de Entidades e Metadata do Doctrine
  */
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/Models/Entity"), $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration($paths, $debug);
+
 /**
  * Array de configurações da nossa conexão com o banco
  */
@@ -22,10 +40,12 @@ $conn = array(
     'driver' => 'pdo_sqlite',
     'path' => __DIR__ . '/db.sqlite',
 );
+
 /**
  * Instância do Entity Manager
  */
 $entityManager = EntityManager::create($conn, $config);
+
 /**
  * Coloca o Entity manager dentro do container com o nome de em (Entity Manager)
  */
--- index.php
+++ index.php
@@ -1,59 +1,121 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
+use App\Models\Entity\Book;
 use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
 
 require './bootstrap.php';
 
 /**
  * Lista de todos os livros
+ * @request curl -X GET http://localhost:8000/book
  */
 $app->get('/books', function (Request $request, Response $response) use ($app) {
-    $return = $response->withJson(['msg' => 'Lista de Livros'], 200)
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $books = $booksRepository->findAll();
+    $return = $response->withJson($books, 200)
         ->withHeader('Content-type', 'application/json');
     return $return;
 });
 
 /**
  * Retornando mais informações do livro informado pelo id
+ * @request curl -X GET http://localhost:8000/book/1
  */
 $app->get('/books/{id}', function (Request $request, Response $response) use ($app) {
     $route = $request->getAttribute('route');
-    $id = $route->getArgument('id');    
-    $return = $response->withJson(['msg' => "Exibindo o livro {$id}"], 200)
+    $id = $route->getArgument('id');
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $book = $booksRepository->find($id);        
+    $return = $response->withJson($book, 200)
         ->withHeader('Content-type', 'application/json');
     return $return;
 });
 
 /**
- * Cadastra um novo Livro
+ * Cadastra um novo <Livro></Livro>
+ * @request curl -X POST http://localhost:8000/book -H "Content-type: application/json" -d '{"name":"O Oceano no Fim do Caminho", "author":"Neil Gaiman"}'
  */
 $app->post('/books', function (Request $request, Response $response) use ($app) {
-    $return = $response->withJson(['msg' => "Cadastrando um livro"], 201)
+
+    $params = (object) $request->getParams();
+    /**
+     * Pega o Entity Manager do nosso Container
+     */
+    $entityManager = $this->get('em');
+
+    /**
+     * Instância da nossa Entidade preenchida com nossos parametros do post
+     */
+    $book = (new Book())->setName($params->name)->setAuthor($params->author);
+
+    /**
+     * Persiste a entidade no banco de dados
+     */
+    $entityManager->persist($book);
+    $entityManager->flush();
+    $return = $response->withJson($book, 201)
         ->withHeader('Content-type', 'application/json');
     return $return;
 });
 
 /**
  * Atualiza os dados de um livro
+ * @request curl -X PUT http://localhost:8000/book/14 -H "Content-type: application/json" -d '{"name":"Deuses Americanos", "author":"Neil Gaiman"}'
  */
 $app->put('/books/{id}', function (Request $request, Response $response) use ($app) {
+    /**
+     * Pega o ID do livro informado na URL
+     */
     $route = $request->getAttribute('route');
-    $id = $route->getArgument('id');    
-    $return = $response->withJson(['msg' => "Modificando o livro {$id}"], 200)
+    $id = $route->getArgument('id');
+    /**
+     * Encontra o Livro no Banco
+     */ 
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $book = $booksRepository->find($id);   
+    /**
+     * Atualiza e Persiste o Livro com os parâmetros recebidos no request
+     */
+    $book->setName($request->getParam('name'))
+         ->setAuthor($request->getParam('author'));
+    /**
+     * Persiste a entidade no banco de dados
+     */
+    $entityManager->persist($book);
+    $entityManager->flush();        
+    
+    $return = $response->withJson($book, 200)
         ->withHeader('Content-type', 'application/json');
     return $return;
 });
 
 /**
  * Deleta o livro informado pelo ID
+ * @request curl -X DELETE http://localhost:8000/book/3
  */
 $app->delete('/books/{id}', function (Request $request, Response $response) use ($app) {
+    /**
+     * Pega o ID do livro informado na URL
+     */
     $route = $request->getAttribute('route');
-    $id = $route->getArgument('id');    
+    $id = $route->getArgument('id');
+    /**
+     * Encontra o Livro no Banco
+     */ 
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $book = $booksRepository->find($id);   
+    /**
+     * Remove a entidade
+     */
+    $entityManager->remove($book);
+    $entityManager->flush(); 
     $return = $response->withJson(['msg' => "Deletando o livro {$id}"], 204)
         ->withHeader('Content-type', 'application/json');
     return $return;
 });
-
 $app->run();
